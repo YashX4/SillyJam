fd006e0b8d1133296a3addc6f4e2e13fc4db1bd5
oto <13265059+otomist@users.noreply.github.com>
2026-06-22 22:18:01 -0400

    remove wildcard syntax

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 import asyncio
-from pyray import *
+import pyray as rl
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 450
 TARGET_FPS = 60
 WINDOW_TITLE = "spin to win game"
 async def main():
-    init_window(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE)
-    set_target_fps(TARGET_FPS)
+    rl.init_window(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE)
+    rl.set_target_fps(TARGET_FPS)
 
-    while not window_should_close():
-        begin_drawing()
-        clear_background(RAYWHITE)
-        draw_text("Hello", 300, 200, 32, DARKGRAY)
-        end_drawing()
+    while not rl.window_should_close():
+        rl.begin_drawing()
+        rl.clear_background(rl.RAYWHITE)
+        rl.draw_text("Hello", 300, 200, 32, rl.DARKGRAY)
+        rl.end_drawing()
 
         await asyncio.sleep(0)
 
-    close_window()
+    rl.close_window()
 
 asyncio.run(main())
