8829b56e5ac9d427d70cdc63402cd16c30a5b733
oto <13265059+otomist@users.noreply.github.com>
2026-06-22 21:25:29 -0400

    pybag dev dep

--- main.py
+++ main.py
@@ -1,15 +1,22 @@
+import asyncio
 from pyray import *
 
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 450
+TARGET_FPS = 60
+WINDOW_TITLE = "spin to win game"
+async def main():
+    init_window(SCREEN_WIDTH, SCREEN_HEIGHT, WINDOW_TITLE)
+    set_target_fps(TARGET_FPS)
 
-init_window(SCREEN_WIDTH, SCREEN_HEIGHT, "Hello raylib from Python")
-set_target_fps(60)
+    while not window_should_close():
+        begin_drawing()
+        clear_background(RAYWHITE)
+        draw_text("Hello", 300, 200, 32, DARKGRAY)
+        end_drawing()
 
-while not window_should_close():
-    begin_drawing()
-    clear_background(RAYWHITE)
-    draw_text("Hello raylib!", 260, 200, 32, DARKGRAY)
-    end_drawing()
+        await asyncio.sleep(0)
 
-close_window()
+    close_window()
+
+asyncio.run(main())
