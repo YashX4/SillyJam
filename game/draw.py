b4138dcecf4fbcc8209e11355724d9f79100bae2
oto <13265059+otomist@users.noreply.github.com>
2026-06-23 13:49:11 -0400

    remove scale of test animation

--- game/draw.py
+++ game/draw.py
@@ -15,7 +15,7 @@ def draw(state: GameState, alpha: float) -> None:
     if state.sprite is not None:
         sprite_x = 40
         sprite_y = 40
-        state.sprite.draw(sprite_x, sprite_y, scale=4.0)
+        state.sprite.draw(sprite_x, sprite_y)
 
     if state.show_grid:
         draw_grid()
